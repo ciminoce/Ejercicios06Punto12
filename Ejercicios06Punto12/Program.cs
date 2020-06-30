243b94be5a9229ead21c73508a7d6fe8e5f9991b
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2020-06-30 19:08:54 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicios06Punto12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicios06Punto12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title="Hallar nros primos";
+        }
+    }
+}

70182f391bae466a4bc8f5787774bac9374692c5
Carlos Enrique Cimino <ciminoce@yahoo.com.ar>
2020-06-30 19:25:31 -0300

    Ejercicio completo

--- Ejercicios06Punto12/Program.cs
+++ Ejercicios06Punto12/Program.cs
@@ -6,7 +6,39 @@ namespace Ejercicios06Punto12
     {
         static void Main(string[] args)
         {
-            Console.Title="Hallar nros primos";
+            Console.Title = "Hallar nros primos";
+            int nro;
+            bool noSalir = true;
+            do
+            {
+                Console.Write("Ingrese un nro entero:");
+                if (int.TryParse(Console.ReadLine(), out nro))
+                {
+                    noSalir = false;
+                }
+                else
+                {
+                    Console.WriteLine("Error al ingresar el nro entero");
+                }
+
+            } while (noSalir);
+
+            for (int i = 1; i <= nro; i++)
+            {
+                Console.WriteLine(VerPrimo(i) ? $"{i} es primo" : $"{i} no es primo");
+            }
+            Console.ReadLine();
+        }
+        private static bool VerPrimo(int numero)
+        {
+            for (int divisor = 2; divisor <= numero-1; divisor++)
+            {
+                if (numero % divisor==0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
